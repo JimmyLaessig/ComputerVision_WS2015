741bef87075576217faf2c1ac66bd812daf9d975
Bernhard <e0828592@student.tuwien.ac.at>
2015-11-21 01:35:25 +0100

    almost finished knn

--- Excercise 1/Assignment2.m
+++ Excercise 1/Assignment2.m
@@ -1,99 +1,111 @@
 function Assignment2
+
 img = im2double(imread('Data\mm.jpg'));
-kNN(img, 2, true);
-end
 
-
-function kNN(img, k, useSpatial)
-% Input
-% img -> the image to be classified
-% k -> number of centroids
-% useSpatial -> determines whether to use spatial information or not
-% OUTPUT
-% classification -> class labels for all elements in img
-
-% Create emtpy 2D information
-
-%% Create Feature vector
-[width, height] = size(img(:,:,1));
-numSamples = width * height;
-
-tmp = img(:,:,1);
-r = tmp(:);
-tmp = img(:,:,1);
-g = tmp(:);
-tmp = img(:,:,1);
-b = tmp(:);
-
-samples = [r g b];
-if(useSpatial == true)
-    
-    x = 1:1:width;
-    x = repmat(x, 1, height);
-    x = x' / width;
-    
-    y = 1:1:height;
-    y = repmat(y, 1, width);
-    y = y' / height;
-    
-    samples = [r g b x y];
-end
-
-numFeatures = size(samples, 2);
-%% Perform kMeans
-classification = zeros(width * height, 1);
-
-
-% Initialize rnd centroids
-rng(0,'twister');
-centroids = rand(k, numFeatures);
-changed = true;
-
-% Repeat as long as the centroids change
-while(changed == true)
-    
-    % Do kNN-Classification for each sample using the Centroids
-    for i=1:numSamples
-        distance = Inf;
-        for j=1:k
-            currentDistance = euclid(samples(i,:), centroids(j,:));
-            if ( currentDistance < distance)
-                distance = currentDistance;
-                classification(i) = j;
-            end
-        end
-    end
-    % Calculate new Centroids
-    newCentroids = calculateCentroids(classification, samples, k, numFeatures, numSamples);
-    % Determine if Centroids have changed
-    %centroidsChanged(centroids, newCentroids, 0.1);
-    changed = centroidsChanged(centroids, newCentroids, 0.1);
-    % Apply new Ventroids if changed
-    if(changed == true)
-        centroids = newCentroids;
-    end
-end
-
-% Plot the result
+% Create Feature Vector
+features = createFeatureVector(img, true);
+% Perform kNN
+[classification, centroids] = kNN(features, 2, 1);
+% Plot Results
 plotResults(classification, img, centroids);
 end
 
-%% Determines if the new centroids differ from the old centroids, capped by threshold
-function[changed] = centroidsChanged(oldCentroids, newCentroids, threshold)
-% TODO: Implement this function correctly
-changed = false;
+
+function [features] = createFeatureVector(img, useSpatial)
+% INPUT
+% img -> the image to be converted into a 2D feature vector
+% useSpatial-> true if the coordinates should be treated as features too
+%% Create Feature vector
+[height, width, ~] = size(img);
+
+tmp = img(:,:,1);
+r = tmp(:);
+tmp = img(:,:,2);
+g = tmp(:);
+tmp = img(:,:,3);
+b = tmp(:);
+
+features = [r g b];
+if(useSpatial == true)
+    
+    % Create array for all x (height) indices 
+    % The array for a 3x2 image look like [1,2,3,1,2,3]
+    x = (1:1:height) / height;
+    x = repmat(x, width, 1)';   % For each row
+    
+    % Create array for all y (width) indices
+    % The array for a 3x2 image look like [1,1,1,2,2,2]
+
+    y = (1:1:width)' / width;
+    y = repmat(y, 1, height)';
+   
+    features = [r g b x(:) y(:)];
+end
 end
 
 
-%% Calculates the euclidean distance between sample1 and sample2
-function[distance] = euclid(sample1, sample2)
-distance = norm(sample1 - sample2);
+function[classification, centroids] = kNN(samples, k, maxIterations)
+% Input
+% img -> the image to be classified
+% numClasses -> number of classes
+% useSpatial -> determines whether to use spatial information or not
+% maxIterations -> number of maximum iterations if the algorithm does not
+% convert
+% OUTPUT
+% classification -> class labels for all elements in img
+% centroids -> the centroid data points of each cluster
+
+% Number of features
+[numSamples, numFeatures] = size(samples);
+% Create empty classifications
+classification = NaN(numSamples, 1);
+distances = NaN(numSamples, k);
+% Create random centroids
+%rng(2,'twister');
+centroids = rand(k, numFeatures);
+
+
+% Repeat as long as the centroids change
+for i = 1:maxIterations
+    disp(['Iteration: ', sprintf('%d', i) ]);
+    % Calculate Distance for each sample to each centroid
+    for class=1:k
+        distances(:, class) = calcDistances(samples, centroids(class,:), numSamples);
+    end
+    
+    % Get Min distances and the indices (= class Label)
+    [~, classification] = min(distances,[],  2);       
+       
+    % Calculate new Centroids
+    newCentroids = calculateCentroids(classification, samples, k, numSamples, numFeatures);
+    %
+    % Terminate algorithm if no change occured or max iterations is reached
+    if(centroidsChanged(centroids, newCentroids, 0.1) == false)
+        break;
+    end
+    % Apply new Centroids
+    centroids = newCentroids;
+    
+end
+end
+
+
+%% Calculates the euclidean distance between sample1 and sample3
+function[distances] = calcDistances(samples, centroid, numSamples)
+
+tmp = samples - repmat(centroid, numSamples, 1);
+distances = sqrt(sum(abs(tmp).^2,2));
+
 end
 
 
 %% Calculates the centroids based on the classification and samples
-function[centroids] = calculateCentroids(classification, samples, k, numFeatures, numSamples)
+function[centroids] = calculateCentroids(classification, samples, k, numSamples, numFeatures)
+
+% Store the number of samples per class
 numClasses = zeros(k, 1);
+
+% Store the new centroids
 centroids = zeros(k, numFeatures);
 
 % Accumulate Samples from each class
@@ -110,32 +122,39 @@ end
 end
 
 
+%% Determines if the new centroids differ from the old centroids, capped by threshold
+function[changed] = centroidsChanged(oldCentroids, newCentroids, threshold)
+% TODO: Implement this function correctly
+changed = true;
+end
+
+
 %% Plots the image and the kMeans clustered image next to another in a figure
 function plotResults(classification, image, centroids)
+
+kMeansImage = zeros(size(image));
+[height, width, ~] = size(image);
+
+numSamples = height * width;
+
+for i=0:numSamples - 1
+    
+    x = mod(i , height) + 1;
+    y = floor(i / height) + 1;
+    
+    class = classification(i+1);
+    
+        kMeansImage(x, y, 1) = centroids(class, 1);
+        kMeansImage(x, y, 2) = centroids(class, 2);
+        kMeansImage(x, y, 3) = centroids(class, 3);
+end
+
 figure;
 subplot(1, 2, 1);
 imshow(image);
 title('Color Image');
-kMeansImage = zeros(size(image));
-[width, height] = size(image(:,:,1));
-numFeatures = size(classification, 2);
-
-for i=1:numFeatures
-    class = classification(i);
-    x = mod(i, width);
-    y = 1 + fix(i / width);
-    centroids(class, 1);
-   % kMeansImage(x, y, 1) = centroids(class, 1);
-   % kMeansImage(x, y, 2) = centroids(class, 2);
-   % kMeansImage(x, y, 3) = centroids(class, 3);
-    kMeansImage(x, y, 1) = 0.5;
-    kMeansImage(x, y, 2) = 0.5;
-    kMeansImage(x, y, 3) = 0.5;
-end
 
 subplot(1, 2, 2);
 imshow(kMeansImage);
 title(['kMeans : k = ' , sprintf('%d', size(centroids, 2))]);
 end
-
-
