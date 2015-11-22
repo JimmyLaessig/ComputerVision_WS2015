4e439381985d39f51ff85145ff4cf9355f294a4c
Andrea Gangl <e1025756@student.tuwien.ac.at>
2015-11-22 19:41:30 +0100

    Added Assignment3

--- Excercise 1/Assignment3.m
+++ Excercise 1/Assignment3.m
@@ -9,20 +9,48 @@ function logBlobDetector( img )
 
 sigma = 2; % initial scale
 k = 1.25; % multiplication value
-scale_space = zeros(size(img,1),size(img,2),10);
+level = 10;
+scale_space = zeros(size(img,1),size(img,2),level);
+max_space = scale_space;
+threshold = 0.1;
+suppression_size = 10;
 
-for i = sigma : k : 10 
-   
-    filter_size =  2*ceil(3*sigma)+1; % filter size
-    log_filter = fspecial('log', filter_size, sigma) * sigma^2;
+for i = 1 : level 
+    sigma2 = sigma * k^(i-1);
+    filter_size =  2*ceil(3*sigma2)+1; % filter size
+    log_filter = fspecial('log', filter_size, sigma2) * sigma2^2;
     filtered = imfilter(img, log_filter, 'same', 'replicate');
-    %scale_space(:,:,i) = filtered;
-    
-    sigma = i;
+    filtered = filtered .^2;
+    scale_space(:,:,i) = filtered;   
 end
 
-rad = sigma * sqrt(2);
-show_all_circles(img, rad,rad,rad);
+%% non-maximum suppression
+
+%non max suppression for each scale_space
+for i = 1:level
+    max_space(:,:,i) = ordfilt2(scale_space(:,:,i), suppression_size^2, ones(suppression_size));
+end
+
+%non max suppression between scales and threshold
+for i = 1:level
+    max_space(:,:,i) = max(max_space(:,:,max(i-1,1):min(i+1,level)),[],3);
+end
+max_space = max_space .* (max_space == scale_space);
+
+cx = [];   
+cy = [];   
+rad = [];
+for i = 1 : level
+    [rows, cols] = find(max_space(:,:,i) >= threshold);
+    tmp_rad = sigma * k^(i-1) * sqrt(2); 
+    tmp_rad = repmat(tmp_rad, length(rows), 1);
+    cx = [cx; cols];
+    cy = [cy; rows];
+    rad = [rad; tmp_rad];
+end
+
+show_all_circles(img, cx, cy, rad);
+
 end
 
 function show_all_circles(I, cx, cy, rad, color, ln_wid)
