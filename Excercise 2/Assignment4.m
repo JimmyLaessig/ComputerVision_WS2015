f0cbd708b3a10b24388465926f217ee0a555155c
Bernhard <e0828592@student.tuwien.ac.at>
2016-01-13 17:15:19 +0100

    added homography multiplication

--- Excercise 2/Assignment4.m
+++ Excercise 2/Assignment4.m
@@ -52,6 +52,8 @@ for i = 1:count
 end
 
 %% TASK B - Interest Point Matching and Image Registration
+% Store transform and number of inliers for each iteration
+transforms = cell(count-1, 1);
 
 for i = 1:count-1
     [matches,scores] = vl_ubcmatch(descriptorArray{i}, descriptorArray{i+1});
@@ -76,8 +78,6 @@ for i = 1:count-1
     set(fig, 'name', ['Matching without RANSAC: ', name, num2str(i), ' to ', name, num2str(i+1)]);
     
     %% Perform RANSAC scheme
-    % Store transform and number of inliers for each iteration
-    TFORM = 0;
     num_inliers = 0;
     
     for j=1:n
@@ -86,31 +86,89 @@ for i = 1:count-1
         samples2 = matching_points2(indices, :);
         
         try
-            % Estimate homography
+            % Estimate homography between two consequtive images and store
+            % it for later use
             TFORM_current = cp2tform(samples1, samples2, 'projective');
             % Transform matching points
             [x, y]= tformfwd(TFORM_current, matching_points1(:,1), matching_points1(:,2));
             transformed_matching_points_1 = horzcat(x,y);
+            
+            % Determine inlier by comparing euclidic distances between the
+            % old and transformed feature points to a certain threshold (e.g. 5)
+            
             % Calculate Euclidean distances
             distances = (sum(((transformed_matching_points_1-matching_points2).^2), 2)).^0.5;
             % Remove all values over the threshold
             distances(distances >= 5) = 0;
             % Set all values beneath the threshold to 1
             distances(distances ~=0) = 1;
-            
             num_inliers_current = sum(distances);
             
+            % If the current homography has more inliers than the previous,
+            % it will be the transform of choice
             if(num_inliers_current > num_inliers)
                 num_inliers = num_inliers_current;
-                TFORM  = TFORM_current;
-            end                   
+                transforms{i}  = TFORM_current;
+            end
+        catch
         end
     end
-    % Transform the image onto the second image using the calculated TFORM
+end
+
+% Transform the images onto the second image using the calculated transforms
+% The figures show the image transformed into the space of the
+% consecutive image space. Therefore all outlying data is cut off and
+% only overlapping parts are visible
+figure('name', [name, ': Images transformed onto consecutive neighbor'], 'position', [0, 0, 1920, 600]);
+for i = 1:count-1
+    subplot(1, count-1, i);
+    
     [height1, width1] = size(imagesRGB(:,:,1,i));
     [height2, width2] = size(imagesRGB(:,:,1,i+1));
     scale = [height1 / height2, width1 / width2];
-    img = imtransform(imagesRGB(:,:,:,i), TFORM, 'XData',[1 width2], 'YData',[1 height2], 'XYScale', scale);
+    img = imtransform(imagesRGB(:,:,:,i), transforms{i}, 'XData',[1 width2], 'YData',[1 height2], 'XYScale', scale);
+    imshow(img);
+    title([num2str(i), ' onto ', num2str(i+1)]);
+end
+
+%% Image Stitching
+
+% Reference image index
+refIndex = ceil(count / 2);
+for i = 1:count
+    
+    % Initialize Transform as identity matrix(Creating identity matrix)
+    transform = maketform('projective', [1 0 0; 0 1 0; 0 0 1]);
+    
+    if (i == refIndex)
+        % Do not transform the reference image since all other images are
+        % aligned to it
+        continue;
+        
+    elseif (i < refIndex)
+        % Index is smaller then reference index (left of it), use regular transforms
+        disp('Creating new Transform');
+        % Accumulate all transforms in the sequence up to the reference index
+        for j= refIndex-1:-1:i
+            disp(['transform ', num2str(j), ',' num2str(j+1)]);
+            transform.tdata.T = transform.tdata.T * transforms{j}.tdata.T;
+        end
+        
+    elseif (i > refIndex)
+        % Index is larger then reference index (right of it), use inverse transforms
+        disp('Creating new Transform');
+        % Accumulate all transforms in the sequence from the reference index to the current index
+        for j=refIndex:i-1
+            disp(['invTransform ', num2str(j), ',' num2str(j+1)]);
+            transform.tdata.Tinv = transform.tdata.Tinv * transforms{j}.tdata.Tinv;
+        end
+        % Set the inverse as regular matrix
+        transform.tdata.T = transform.tdata.Tinv;       
+    end
+    
+    % Calculate new inverse Transform (just in case)
+    transform.tdata.tInv = inv(transform.tdata.T);
+    % TODO: Transform image here with the calculated TFORM
     
 end
 end
