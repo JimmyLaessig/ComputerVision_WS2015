11477c7c2485cad900b6ad2c3d70567d5efeeb4c
Andrea Gangl <e1025756@student.tuwien.ac.at>
2016-01-14 19:38:18 +0100

    small update assignemnt 5

--- Excercise 2/Assignment5.m
+++ Excercise 2/Assignment5.m
@@ -3,3 +3,8 @@
 
 %addpath('E:\Dateien\Eigene Dokumente\Studium\CV\vlfeat-0.9.19\toolbox','-end');
 %vl_setup();
+
+%% TODO: rename to main.m
+C = BuildVocabulary(folder, num_clusters);
+[training, group] = BuildKNN(folder,C);
+conf_matrix = ClassifyImages(folder,C,training,group);
