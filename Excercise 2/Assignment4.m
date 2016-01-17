484e934baa3bd7af9d592a0640caf0b23d13689c
Bernhard <e0828592@student.tuwien.ac.at>
2016-01-17 18:58:16 +0100

    added basic stitching

--- Excercise 2/Assignment4.m
+++ Excercise 2/Assignment4.m
@@ -1,9 +1,12 @@
 function Assignment4
-
-ImageStitching('campus', 5, 'jpg');
+% Before running this assignment, make sure that you call vl_setup in order
+% to initialize vlfeat framework.
+% Runs the Image Stiching assignment with the given image sequence located in
+% ass4_data folder
+ImageStitching('campus', 5, 'jpg', 100);
 end
 
-function ImageStitching(name, count, datatype)
+function ImageStitching(name, count, datatype, RANSAC_num_iterations)
 % name     ... the base name of the image sequence
 % count    ... the number of images in the sequence
 % datatype ... the image data type
@@ -11,24 +14,17 @@ function ImageStitching(name, count, datatype)
 %% TASK Prerequisites - Read data into memory
 % Image sequence is stored in two arrays, one containing the reference
 % images in RGB, the other the processing images in greyscale
-path =  strcat('ass4_data\', name, '1.', datatype);
-n = 100;
-tmp = imread(path);
 
-imagesRGB = zeros(size(tmp,1),size(tmp,2), 3,count);
-imagesGREY = zeros(size(tmp,1), size(tmp,2), count);
+imagesRGB = cell(count, 1);
+imagesGREY = cell(count, 1);
 imagesInfo = cell(count, 1);
 
-imagesRGB(:,:,:,1) = im2double(tmp);
-imagesGREY(:,:,1) = rgb2gray(tmp);
-imagesInfo{1} = imfinfo(path);
-
-for i=2:count
+for i=1:count
     path =  strcat('ass4_data\', name, num2str(i),'.', datatype);
     tmp = imread(path);
-    imagesRGB(:,:,:,i) = im2double(tmp);
+    imagesRGB{i} = im2double(tmp);
     % Do NOT normalize greyscale image to the range of 0-1 since vl_sift needs the range to be normalized to 0-255
-    imagesGREY(:,:,i) = rgb2gray(tmp);
+    imagesGREY{i} = rgb2gray(tmp);
     imagesInfo{i} = imfinfo(path);
 end
 
@@ -41,13 +37,13 @@ descriptorArray = cell(count, 1);
 
 for i = 1:count
     
-    [features, descriptors] = vl_sift((single(imagesGREY(:,:,i))));
+    [features, descriptors] = vl_sift((single(imagesGREY{i})));
     featureArray{i} = features;
     descriptorArray{i} = descriptors;
     
     % Plot Sift Features into figure
     subplot(1, count, i);
-    imshow(imagesRGB(:,:,:,i));
+    imshow(imagesRGB{i});
     h1 = vl_plotframe(featureArray{i});
     set(h1, 'color','r','linewidth', 2);
     title(strcat(name, num2str(i),'.', datatype));
@@ -58,7 +54,7 @@ end
 transforms = cell(count-1, 1);
 
 for i = 1:count-1
-    [matches,scores] = vl_ubcmatch(descriptorArray{i}, descriptorArray{i+1});
+    [matches,~] = vl_ubcmatch(descriptorArray{i}, descriptorArray{i+1});
     
     % SIFT Features for the two images
     features1 = featureArray{i};
@@ -76,13 +72,13 @@ for i = 1:count-1
     matching_points2 = points2(:, indices2)';
     
     % Plot Matches
-    fig = match_plot(imagesRGB(:,:,:,i), imagesRGB(:,:,:,i+1), matching_points1, matching_points2);
+    fig = match_plot(imagesRGB{i}, imagesRGB{i+1}, matching_points1, matching_points2);
     set(fig, 'name', ['Matching without RANSAC: ', name, num2str(i), ' to ', name, num2str(i+1)]);
     
     %% Perform RANSAC scheme
     num_inliers = 0;
     
-    for j=1:n
+    for j = 1:RANSAC_num_iterations
         % Randomly choose four matches
         [samples1, indices] = datasample(matching_points1, 4);
         samples2 = matching_points2(indices, :);
@@ -132,8 +128,10 @@ for i = 1:count-1
     width2 = imagesInfo{i+1}.Width;
     
     scale = [height1 / height2, width1 / width2];
-    img = imtransform(imagesRGB(:,:,:,i), transforms{i}, 'XData',[1 width2], 'YData',[1 height2], 'XYScale', scale);
-    imshow(img);
+    img_transformed = imtransform(imagesRGB{i}, transforms{i}, 'XData',[1 width2], 'YData',[1 height2], 'XYScale', scale);
+    % Create a mask to overlay the transformed image onto the base image
+    mask = (img_transformed > 0);
+    imshow(mask .* img_transformed + ~mask .* imagesRGB{i+1});
     title([num2str(i), ' onto ', num2str(i+1)]);
 end
 
@@ -145,19 +143,17 @@ homographies = cell(count, 1);
 for i = 1:count
     
     % Initialize Transform as identity matrix(Creating identity matrix)
-    transform = maketform('projective', [1 0 0; 0 1 0; 0 0 1]);
+    transform =  [1 0 0; 0 1 0; 0 0 1];
+    % Do not transform the reference image since all other images are
+    % aligned to it
     
-    if (i == refIndex)
-        % Do not transform the reference image since all other images are
-        % aligned to it
-        
-    elseif (i < refIndex)
+    if (i < refIndex)
         % Index is smaller then reference index (left of it), use regular transforms
         disp('Creating new Transform');
         % Accumulate all transforms in the sequence up to the reference index
         for j= refIndex-1:-1:i
             disp(['transform ', num2str(j), ',' num2str(j+1)]);
-            transform.tdata.T = transform.tdata.T * transforms{j}.tdata.T;
+            transform = transform * transforms{j}.tdata.T;
         end
         
     elseif (i > refIndex)
@@ -166,39 +162,53 @@ for i = 1:count
         % Accumulate all transforms in the sequence from the reference index to the current index
         for j=refIndex:i-1
             disp(['invTransform ', num2str(j), ',' num2str(j+1)]);
-            transform.tdata.Tinv = transform.tdata.Tinv * transforms{j}.tdata.Tinv;
+            transform = transform * transforms{j}.tdata.Tinv;
         end
-        % Set the inverse as regular matrix
-        transform.tdata.T = transform.tdata.Tinv;
     end
     
-    % Calculate new inverse Transform (just in case)
-    transform.tdata.tInv = inv(transform.tdata.T);
-    
-    homographies{i} = transform;
-    
+    % Create new TFORM with the calculated transformation matrix
+    homographies{i} = maketform('projective', transform);
 end
 
 % Estimate dimensions of output image
+% Transform the corners of all images onto the space of the centered base
+% image
 corners = zeros(count * 4, 3);
 for i = 1:count
-    height = imagesInfo{i}.Height;
-    width = imagesInfo{i}.Width;
     
-    corners ((i-1) * 4 + 1, :) = homographies{i}.tdata.T * [1; 1; 1];
-    corners ((i-1) * 4 + 2, :) = homographies{i}.tdata.T * [height; 1; 1];
-    corners ((i-1) * 4 + 3, :) = homographies{i}.tdata.T * [1; width; 1];
-    corners ((i-1) * 4 + 4, :) = homographies{i}.tdata.T * [height; width; 1];    
-     
+    x = imagesInfo{i}.Height;
+    y = imagesInfo{i}.Width;
+    
+    corners ((i-1) * 4 + 1, :) = [1 1 1] * homographies{i}.tdata.T;
+    corners ((i-1) * 4 + 2, :) = [x 1 1] * homographies{i}.tdata.T;
+    corners ((i-1) * 4 + 3, :) = [1 y 1] * homographies{i}.tdata.T;
+    corners ((i-1) * 4 + 4, :) = [x y 1] * homographies{i}.tdata.T ;
+    
+end
+% Perform projective division
+corners(:,1) = corners(:,1) ./ corners(:,3);
+corners(:,2) = corners(:,2) ./ corners(:,3);
+corners(:,3) = corners(:,3) ./ corners(:,3);
+
+minWidth =  floor(min(corners(:,1)));
+maxWidth =   ceil(max(corners(:,1)));
+minHeight = floor(min(corners(:,2)));
+maxHeight =  ceil(max(corners(:,2)));
+
+% Determine final dimensions using the min and max values of all
+% transformed corners
+image_width = maxWidth - minWidth;
+image_height = maxHeight - minHeight;
+
+% Init final image with the calculated dimensions
+image_stitched = zeros(image_height + 1, image_width + 1, 3);
+% Transform all images onto the base plane
+for i = 1:count
+    img_transformed = imtransform(imagesRGB{i}, homographies{i}, 'XData',[minWidth maxWidth], 'YData',[minHeight maxHeight]);
+    %mask = (img_transformed > 0);
+    image_stitched = image_stitched + img_transformed;
 end
 
-%corners(:,1) = corners(:,1) ./ corners(:,3);
-%corners(:,2) = corners(:,2) ./ corners(:,3);
-%corners(:,3) = corners(:,3) ./ corners(:,3);
-
-minX = min(corners(:,1));
-maxX = max(corners(:,1));
-minY = min(corners(:,2));
-maxY = max(corners(:,2));
-
+figure('name', 'Stitched image');
+imshow(image_stitched);
 end
